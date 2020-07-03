af30dff34cf3f36b8727254ae7a12c40c2d72f47
snpushpi <55248448+snpushpi@users.noreply.github.com>
2020-07-03 15:52:00 -0400

    commit

---
+++ parantheses.py
@@ -0,0 +1,26 @@
+'''
+	 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+	For example, given n = 3, a solution set is:
+	[
+	  "((()))",
+	  "(()())",
+	  "(())()",
+	  "()(())",
+	  "()()()"
+	]
+'''
+def generateParantheses(n):
+    result = []
+
+    def backtracking(S, left, right):
+        if len(S) == 2*n:
+        	result.append(S)
+        	return 
+
+        if left < n:
+            backtracking(S+'(', left+1, right)
+        if right < left:
+        	backtracking(S+')', left, right+1)
+    backtracking('', 0, 0)
+    return result
+print(generateParantheses(4))
