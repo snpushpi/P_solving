a6df72bd9e24bf15ae3b40af052b4779eb2e5a67
snpushpi <55248448+snpushpi@users.noreply.github.com>
2020-07-14 21:15:39 -0400

    prac

---
+++ zigzag_level_traversal.py
@@ -0,0 +1,35 @@
+'''
+    Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+    For example:
+    Given binary tree [3,9,20,null,null,15,7],
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    return its zigzag level order traversal as:
+    [
+      [3],
+      [20,9],
+      [15,7]
+    ]
+'''
+class Tree():
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+def zigzag(root_node):
+    level = [root_node]
+    result = []
+    while level:
+        add_list =[]
+        for elt in level:
+            if elt.left not None:
+                add_list.append(elt.left)
+            if elt.right not None:
+                add_list.append(elt.right)
+        result.append(add_list)
+        level = add_list
+    return result
+
