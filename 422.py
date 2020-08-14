1f5b5c4031c86c9df55bd784921e23c36396dd72
snpushpi <55248448+snpushpi@users.noreply.github.com>
2020-08-14 14:38:44 -0400

    442

---
+++ 422.py
@@ -0,0 +1,19 @@
+'''
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+Find all the elements that appear twice in this array.
+Could you do it without extra space and in O(n) runtime?
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+Output:
+[2,3]
+''' 
+def main(num_list):
+    result = []
+    for i,num in enumerate(num_list):
+        index = num-1
+        if num_list[index]<0:
+            result.append(index+1)
+        num_list[index]*=-1
+    return result
+print(main([4,3,2,7,8,2,3,1]))
